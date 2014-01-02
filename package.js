f119801cdff491a113c548771cbd41786b05b6b3
pwldp <pawlakdp@gmail.com>
2014-01-02 12:18:13 +0100

    Update package.js

--- package.js
+++ package.js
@@ -9,10 +9,10 @@ Package.on_use(function (api, where) {
   var asset_path = path.join('uikit-2');
   
   //js
-  api.add_files(path.join(asset_path, 'js', 'uikit.min.js'), 'client');
+  api.add_files(path.join(asset_path, 'js', 'uikit.js'), 'client');
   
   //css
-  api.add_files(path.join(asset_path, 'css', 'uikit.min.css'), 'client');
+  api.add_files(path.join(asset_path, 'css', 'uikit.css'), 'client');
 
   //fonts
   api.add_files(path.join(asset_path, 'fonts', 'fontawesome-webfont.eot'), 'client');
